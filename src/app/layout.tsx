5bce68f4f59c0478bdb938b42f80a88e58917a77
Renato Bezerra <renato-bgs@live.com>
2026-05-07 20:41:50 -0300

    feat: integrate Meta Pixel tracking script and noscript fallback into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,6 +110,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-NV6BPT7T');`}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1491713342606274');fbq('track','PageView');`}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=1491713342606274&ev=PageView&noscript=1" alt="" />
+        </noscript>
       </body>
     </html>
   )
